1804f051370a7b88bf34981f0e1880ee9a993937
paulallen825 <paul.allen.neil@gmail.com>
2021-07-09 11:33:32 -0400

    update python exercises

---
+++ data_types_and_variables.py
@@ -0,0 +1,11 @@
+#3 per day
+#little mermaid 3 days
+#brother bear 5 days
+#hercules 1 day
+num_movies_rented = 3
+num_days_rented = 9
+rental_price = 3
+p_p_d = (num_movies_rented * rental_price)
+total_price = p_p_d * num_days_rented
+
+
